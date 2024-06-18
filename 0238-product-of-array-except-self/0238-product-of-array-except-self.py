d77d087e8cc5a1bca10274a155f6f464865dc147
Raj Nathwani <32975582+Natu7@users.noreply.github.com>
2024-06-18 17:33:37 -0500

    Time: 305 ms (8.47%), Space: 26 MB (36.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -12,14 +12,17 @@ class Solution:
                 count += 1
                 total_zero *= i
                 flag = 0
+        print(total_zero)
+        
         i = 0
         while i < len(nums):
+            print(flag)
             if count > 1:
                 l.append(0)
             elif nums[i] == 0: 
                 l.append(total)            
             elif flag == 0:
-                l.append(total_zero//nums[i])
+                l.append(total_zero)
             else:
                 l.append(total//nums[i])
             i += 1
